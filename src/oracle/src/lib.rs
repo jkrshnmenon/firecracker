c9b98f19a4cd0ded52062766fb2129f408c19103
jkrshnmenon <jkrshnmenon@gmail.com>
2023-03-30 16:29:07 -0700

    if kvm_translate fails, we walk the page tables

--- src/oracle/src/lib.rs
+++ src/oracle/src/lib.rs
@@ -5,6 +5,8 @@ use std::io::{
     //Read, 
     Write
 };
+use vm_memory::{GuestMemoryMmap, GuestAddress, Bytes};
+use logger::log_jaeger_warning;
 
 pub const INIT:u64 = 0;
 pub const EXEC:u64 = 1;
@@ -15,6 +17,12 @@ pub const UNMODIFY:u64 = 4;
 pub const HANDLED:u64 = 0;
 pub const STOPPED:u64 = 1;
 pub const CRASHED:u64 = 2;
+
+const PAGESHIFT: u32 = 12;
+const PTSHIFT: u32 = PAGESHIFT;
+const PDSHIFT: u32 = PTSHIFT + 9;
+const PDPSHIFT: u32 = PDSHIFT + 9;
+const _PGDSHIFT: u32 = PDPSHIFT + 9;
 
 
 static mut DOJOSNOOP_CR3: Option<u64> = None;
@@ -31,6 +39,82 @@ const ORACLE_IP: &str = "localhost";
 const ORACLE_PORT: i32 = 31337;
 
 static mut STREAM: Option<TcpStream> = None;
+
+
+fn get_virt_indicies(addr: u64) -> (u64, u64, u64, u64) {
+    let addr = addr >> PAGESHIFT;
+    let pt = (addr >> (0 * 9)) & 0x1ff; 
+    let pd = (addr >> (1 * 9)) & 0x1ff; 
+    let pdp = (addr >> (2 * 9)) & 0x1ff;
+    let pgd = (addr >> (3 * 9)) & 0x1ff;
+    (pgd, pdp, pd, pt)
+}
+
+/// Convert a virtual address into physical
+pub fn pagewalk(gm: GuestMemoryMmap, addr: u64, cr3: u64) -> u64 {
+    let indicies = get_virt_indicies(addr);
+    let (pgd_idx, pdp_idx, pd_idx, pt_idx) = indicies;
+
+    // let pgd_data = read_page(cr3);
+    // let phys_pgd_data = &pgd_data[pgd_idx * 8..(pgd_idx + 1) * 8];
+    let phys_pgd_data = &mut [0u8; 8];
+    // log_jaeger_warning("pagewalk", format!("Reading :{:#016x}", cr3 + pgd_idx * 8).as_str());
+    gm.read_slice(phys_pgd_data, GuestAddress((cr3 & !(0xfff)) + pgd_idx * 8))
+    .expect("Failed to read cr3");
+    let phys_pgd_entry = u64::from_le_bytes(*phys_pgd_data);
+    let phys_pdp_address = phys_pgd_entry & !(0xfff) & !(1 << 63);
+    if phys_pgd_entry == 0 {
+        log_jaeger_warning("pagewalk", "phys_pgd_entry is 0");
+        return 0; 
+    }
+
+    // let pdp_data = read_page(phys_pdp_address);
+    // let phys_pdp_data = &pdp_data[pdp_idx * 8..(pdp_idx + 1) * 8];
+    let phys_pdp_data = &mut [0u8; 8];
+    // log_jaeger_warning("pagewalk", format!("Reading :{:#016x}", phys_pdp_address + pdp_idx * 8).as_str());
+    gm.read_slice(phys_pdp_data, GuestAddress(phys_pdp_address + pdp_idx * 8))
+    .expect("Failed to read phys_pdp_address");
+    let phys_pdp_entry = u64::from_le_bytes(*phys_pdp_data);
+    let phys_pd_address = phys_pdp_entry & !(0xfff) & !(1 << 63);
+    if phys_pdp_entry == 0 {
+        log_jaeger_warning("pagewalk", "phys_pdp_entry is 0");
+        return 0;
+    }
+    if (phys_pdp_entry >> 7) & 1 == 1 {
+        return (phys_pd_address + (addr & ((1 << PDPSHIFT) - 1))) as u64; 
+    }
+
+    // let pd_data = read_page(phys_pd_address);
+    // let phys_pd_data = &pd_data[pd_idx * 8..(pd_idx + 1) * 8];
+    let phys_pd_data = &mut [0u8; 8];
+    // log_jaeger_warning("pagewalk", format!("Reading :{:#016x}", phys_pd_address + pd_idx * 8).as_str());
+    gm.read_slice(phys_pd_data, GuestAddress(phys_pd_address + pd_idx * 8))
+    .expect("Failed to read phys_pd_address");
+    let phys_pd_entry = u64::from_le_bytes(*phys_pd_data);
+    let phys_pt_address = phys_pd_entry & !(0xfff) & !(1 << 63);
+    if phys_pd_entry == 0 {
+        log_jaeger_warning("pagewalk", "phys_pd_entry is 0");
+        return 0;
+    }
+    if (phys_pd_entry >> 7) & 1 == 1 { 
+        return (phys_pt_address + (addr & ((1 << PDSHIFT) - 1))) as u64;
+    }
+
+    // let pt_data = read_page(phys_pt_address);
+    // let phys_pt_data = &pt_data[pt_idx * 8..(pt_idx + 1) * 8];
+    let phys_pt_data = &mut [0u8; 8];
+    // log_jaeger_warning("pagewalk", format!("Reading :{:#016x}", phys_pt_address + pt_idx * 8).as_str());
+    gm.read_slice(phys_pt_data, GuestAddress(phys_pt_address + pt_idx * 8))
+    .expect("Failed to read phys_pt_address");
+    let phys_pt_entry = u64::from_le_bytes(*phys_pt_data);
+    let phys_address = phys_pt_entry & !(0xfff) & !(1 << 63);
+    if phys_pt_entry == 0 {
+        log_jaeger_warning("pagewalk", "phys_pt_entry is 0");
+        return 0;
+    }
+    return (phys_address + (addr & ((1 << PTSHIFT) - 1))) as u64;
+}
+
 
 /// Wrapper for sending messages to Oracle
 fn send_message(msg: &str) -> std::io::Result<()> {
