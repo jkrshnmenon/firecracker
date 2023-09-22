f840d1a0dbf49b60336bc3ee7908fc13c47508bd
Jayakrishna <jkrshnmenon@gmail.com>
2023-09-22 00:05:49 +0000

    remove conditional compiling around debug mode

--- src/vmm/src/builder.rs
+++ src/vmm/src/builder.rs
@@ -302,7 +302,6 @@ fn create_vmm_and_vcpus(
 
     for i in &mut vcpus {
         i.kvm_vcpu.guest_memory_map = Some(guest_memory.clone());
-        #[cfg(target_arch = "x86_64")]
         i.kvm_vcpu.enable_debug();
     }
     let vmm = Vmm {
